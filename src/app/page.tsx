f312598189d576705227110bcdbaa06af01cbdc9
Kuya kevin <kevingilguanzon@gmail.com>
2025-08-06 22:57:06 +0800

    daily update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
           Stay Informed, Stay Safe. Your School&apos;s Daily Weather Update.
         </p>
         <p className="text-xl font-semibold text-center text-gray-600 mb-12">
-          August 6, 2025 Forecast data from Tomorrow.io
+          August 7, 2025 Forecast data from Tomorrow.io
         </p>
 
         {/* School selection dropdown section */}
